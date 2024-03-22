cd4f45910d0146cab6b3baae7272570db9931cec
Tran Xuan Loc <tranxuanloc19920@gmail.com>
2024-03-23 00:28:31 +0700

    read input string from stdin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::io;
+
 fn main() {
-    println!("Hello world");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).ok().expect("Couldn't read line");
+    println!("{}", input);
 }
