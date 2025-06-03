5443e469f18225e4ed459389aaaf2b6ac14397dd
Tran Xuan Loc <tranxuanloc19920@gmail.com>
2025-06-03 23:04:32 +0700

    stop when hit q

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,10 @@ fn main() -> io::Result<()> {
     let mut stdin = io::stdin().lock();
     let mut byte = [0u8; 1];
 
-    while stdin.read(&mut byte)? == 1 {}
+    while stdin.read(&mut byte)? == 1 {
+        if byte[0] == b'q' {
+            break;
+        }
+    }
     Ok(())
 }
