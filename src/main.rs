253a0a6a0fa6e3a1463f2d9cd0a12dffb16d4fba
Tran Xuan Loc <tranxuanloc19920@gmail.com>
2025-06-03 22:36:56 +0700

    format

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ use std::io;
 
 fn main() {
     let mut input = String::new();
-    io::stdin().read_line(&mut input).ok().expect("Couldn't read line");
+    io::stdin()
+        .read_line(&mut input)
+        .ok()
+        .expect("Couldn't read line");
     println!("{}", input);
 }
